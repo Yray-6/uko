c706dc32e5a1f167d7d08677c9ad9cd0636c982f
Yray-6 <vwaireonobrudu@gmail.com>
2024-05-21 15:34:26 +0100

    dont show on mobile and tabs

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "./ui/Hero";
 
 export default function Home() {
   return (
-   <main className=" bg-primary">
+   <main className=" bg-primary hidden lg:block">
     <section className=" h-screen overflow-hidden">
     <Navbar/>
     <Hero/>
